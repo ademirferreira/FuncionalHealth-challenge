8c5cfb336a63fd78bad4a028d1945c92474ff775
Ademir Ferreira <whoisademir@gmail.com>
2021-07-26 16:33:32 -0300

    Implementação dos endpoints na controller

--- Controllers/ContaCorrenteController.cs
+++ Controllers/ContaCorrenteController.cs
@@ -32,16 +32,63 @@ namespace FuncionalBank.Controllers
             var conta = await _repository.GetConta(numeroDaConta);
             if (conta is null) return NotFound("Conta não encontrada");
 
-            return conta;
+            return Ok(conta);
         }
 
-        [HttpGet("saldo")]
-        public async Task<ActionResult<string>> Saldo(int numeroDaConta)
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult<ContaCorrente>> NovaConta(ContaCorrente conta)
         {
-            var saldo = await _repository.GetSaldo(numeroDaConta);
-            if (saldo is null) return NotFound("Conta não encontrada");
+            var contaCorrente = await _repository.GetConta(conta.Numero);
+            if (contaCorrente is not null) return StatusCode(422, "Já existe uma conta com esse número");
+            var novaConta = await _repository.NovaConta(conta);
 
-            return saldo;
+            return CreatedAtAction("GetConta", new {numeroDaConta = novaConta.Numero}, novaConta);
         }
+
+        [HttpGet("saldo/{numeroDaConta:int}")]
+        public async Task<ActionResult<ContaCorrente>> Saldo(int numeroDaConta)
+        {
+            var conta = await _repository.GetConta(numeroDaConta);
+            if (conta is null) return NotFound("Conta não encontrada");
+            
+            return Ok("Saldo disponível: R$" + conta.Saldo);
+        }
+
+        [HttpPut("depositar/{numeroDaConta:int}")]
+        public async Task<ActionResult<ContaCorrente>> Depositar(int numeroDaConta, decimal valor)
+        {
+            try
+            {
+                var conta = await _repository.GetConta(numeroDaConta);
+                if (conta is null) return NotFound("Conta não encontrada");
+                var contaDeposito = await _repository.Depositar(conta, valor);
+
+                return Ok("Saldo disponível: R$" + contaDeposito.Saldo);
+
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(new {message = e.Message});
+            }
+        }
+
+        [HttpPut("sacar/{numeroDaConta:int}")]
+        public async Task<ActionResult<ContaCorrente>> Sacar(int numeroDaConta, decimal valor)
+        {
+            try
+            {
+                var conta = await _repository.GetConta(numeroDaConta);
+                if (conta is null) return NotFound("Conta não encontrada");
+                var contaSaque = await _repository.Sacar(conta, valor);
+
+                return Ok("Saldo disponível: R$" + contaSaque.Saldo);
+
+            }
+            catch (ArgumentException e)
+            {
+                return BadRequest(new {message = e.Message});
+            }
+        }
+
     }
 }
